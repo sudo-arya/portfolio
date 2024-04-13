0e04c90655e99cd875cb860341472935ab0f6db8
Deepanshu Arya <whoami.arya@gmail.com>
2024-04-13 18:51:02 +0530

    reupdated main.jsx

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // publicDir: "./public/resumes/",
-  // // Other configuration options...
-  // optimizeDeps: {
-  //   include: ["pdfjs-dist/build/pdf.worker.min.js"], // If you're using pdfjs, include it in the optimized dependencies
-  // },
-  // // Configure MIME types if necessary
-  // server: {
-  //   // Specify MIME types for specific file extensions
-  //   mimeTypes: {
-  //     ".pdf": "application/pdf",
-  //   },
-  // },
+  publicDir: "./public/resumes/",
+  // Other configuration options...
+  optimizeDeps: {
+    include: ["pdfjs-dist/build/pdf.worker.min.js"], // If you're using pdfjs, include it in the optimized dependencies
+  },
+  // Configure MIME types if necessary
+  server: {
+    // Specify MIME types for specific file extensions
+    mimeTypes: {
+      ".pdf": "application/pdf",
+    },
+  },
 });
 
