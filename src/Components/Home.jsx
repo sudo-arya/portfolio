6e40d6564ea9b4b92b51a61b7e2177f6644e8e61
Deepanshu Arya <whoami.arya@gmail.com>
2024-04-13 23:25:11 +0530

    added cf account

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -371,7 +371,7 @@ const Home = () => {
 
           {/* spotify blend BUTTON */}
           <a
-            href="https://www.geeksforgeeks.org/user/sudo_arya/"
+            href="https://codeforces.com/profile/TLE_lord"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Twitter"
@@ -379,21 +379,30 @@ const Home = () => {
             onMouseEnter={playSound}
             onMouseLeave={stopSound}
           >
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth={0}
-              viewBox="0 0 24 24"
-              className="absolute right-4 bottom-4 top-auto md:right-8 md:top-8 text-xl text-white"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                height: "100vh",
+              }}
             >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
-            </svg>
-            {/* card element for twitter */}
-            {/* <svg
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth={0}
+                viewBox="0 0 24 24"
+                className="absolute right-4 bottom-4 top-auto md:right-8 md:top-8 text-xl text-white"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path d="M19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+              </svg>
+              {/* card element for twitter */}
+              {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               className="text-4xl md:text-7xl lg:text-9xl text-white"
               height="1em"
@@ -412,8 +421,8 @@ const Home = () => {
               />
             </svg> */}
 
-            {/* card element for gfg */}
-            <svg
+              {/* card element for cf */}
+              {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               className="text-4xl md:text-7xl lg:text-9xl text-white"
               x="0px"
@@ -426,10 +435,34 @@ const Home = () => {
                 fill="#43a047"
                 d="M29.035,24C29.014,23.671,29,23.339,29,23c0-6.08,2.86-10,7-10c3.411,0,6.33,2.662,7,7l2,0l0.001-9	L43,11c0,0-0.533,1.506-1,1.16c-1.899-1.066-3.723-1.132-6.024-1.132C30.176,11.028,25,16.26,25,22.92	c0,0.364,0.021,0.723,0.049,1.08h-2.099C22.979,23.643,23,23.284,23,22.92c0-6.66-5.176-11.892-10.976-11.892	c-2.301,0-4.125,0.065-6.024,1.132C5.533,12.506,5,11,5,11l-2.001,0L3,20l2,0c0.67-4.338,3.589-7,7-7c4.14,0,7,3.92,7,10	c0,0.339-0.014,0.671-0.035,1H0v2h1.009c1.083,0,1.977,0.861,1.999,1.943C3.046,29.789,3.224,32.006,4,33c1.269,1.625,3,3,8,3	c5.022,0,9.92-4.527,11-10h2c1.08,5.473,5.978,10,11,10c5,0,6.731-1.375,8-3c0.776-0.994,0.954-3.211,0.992-5.057	C45.014,26.861,45.909,26,46.991,26H48v-2H29.035z M11.477,33.73C9.872,33.73,7.322,33.724,7,32	c-0.109-0.583-0.091-2.527-0.057-4.046C6.968,26.867,7.855,26,8.943,26H19C18.206,30.781,15.015,33.73,11.477,33.73z M41,32	c-0.322,1.724-2.872,1.73-4.477,1.73c-3.537,0-6.729-2.949-7.523-7.73h10.057c1.088,0,1.975,0.867,2,1.954	C41.091,29.473,41.109,31.417,41,32z"
               ></path>
-            </svg>
+            </svg> */}
+              {/* < style="display: flex; justify-content: center;"> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                id="code-forces"
+                className="text-4xl md:text-7xl lg:text-9xl text-white"
+                x="0px"
+                y="0px"
+                width="50%"
+                height="50%"
+              >
+                <path
+                  fill="#F44336"
+                  d="M24 19.5V12a1.5 1.5 0 0 0-1.5-1.5h-3A1.5 1.5 0 0 0 18 12v7.5a1.5 1.5 0 0 0 1.5 1.5h3a1.5 1.5 0 0 0 1.5-1.5z"
+                ></path>
+                <path
+                  fill="#2196F3"
+                  d="M13.5 21a1.5 1.5 0 0 0 1.5-1.5v-15A1.5 1.5 0 0 0 13.5 3h-3C9.673 3 9 3.672 9 4.5v15c0 .828.673 1.5 1.5 1.5h3z"
+                ></path>
+                <path
+                  fill="#FFC107"
+                  d="M0 19.5c0 .828.673 1.5 1.5 1.5h3A1.5 1.5 0 0 0 6 19.5V9a1.5 1.5 0 0 0-1.5-1.5h-3C.673 7.5 0 8.172 0 9v10.5z"
+                ></path>
+              </svg>
 
-            {/* card element for spotify */}
-            {/* <svg
+              {/* card element for spotify */}
+              {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               enable-background="new 0 0 24 24"
               viewBox="0 0 24 24"
@@ -455,6 +488,7 @@ const Home = () => {
                 ??
               </p>
             </div> */}
+            </div>
           </a>
 
           {/* INSTAGRAM BUTTON */}
