1d6f5d451e219b6fc33fa23cab56fbfd9095b61e
Deepanshu Arya <whoami.arya@gmail.com>
2024-04-13 21:56:56 +0530

    updated home.jsx view resume component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -96,8 +96,8 @@ const Home = () => {
               <path d="M19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
             </svg>
             <div className="">
-              <a
-                href="/DeepanshuArya-Resume"
+              <Link
+                to="/DeepanshuArya-Resume"
                 target="_self"
                 onMouseEnter={playSound}
                 onMouseLeave={stopSound}
@@ -110,7 +110,7 @@ const Home = () => {
                     View CV
                   </p>
                 </div>
-              </a>
+              </Link>
             </div>
           </div>
 
