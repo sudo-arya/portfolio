64efea089febf71b8a30f8380d87e108afa2c446
Deepanshu Arya <whoami.arya@gmail.com>
2024-04-14 14:01:54 +0530

    added redirects in netlify.toml

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./App.css";
-import Home from "./Components/Home.jsx";
-import Resume from "./Components/Resume.jsx";
-import Projects from "./Components/Projects.jsx";
+
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
